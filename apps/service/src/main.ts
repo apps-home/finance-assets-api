dcf006f71e0849373d3f3437830a7e18ce2c2515
Lucasdias067 <lucasdias067@gmail.com>
2026-01-22 18:59:26 -0300

    commit

--- apps/service/src/main.ts
+++ apps/service/src/main.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
+  
   await app.listen(3333)
 }
 
